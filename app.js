d0050f0eefc6380c5a52828f43910023ee5a975e
my name <blueshaperd3@gmail.com>
2024-03-01 02:37:38 +0500

    not done

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ alert (a);
 
 var b = 7 % 4;
 alert (b);
+
+var x = prompt ("Enter your name","Your age is");
+alert (x);
+
+var y = prompt ("Enter your name","Your age is");
+var z = y + 10;
+console.log(z);
+
+var c = +prompt ("Enter your name","Your age is");
+var d = c + 20; 
+console.log(d);
+
+var f = 10;
+document.write("The output is" + "" + f);
