7354c4233668a9f009179929c8be1ebc5d749f0d
my name <blueshaperd3@gmail.com>
2024-03-04 01:11:11 +0500

    onditions apply

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-var a = 10 % 3;
-alert (a);
+//var a = 10 % 3;
+//alert (a);
 
-var b = 7 % 4;
-alert (b);
+//var b = 7 % 4;
+//alert (b);
 
-var x = prompt ("Enter your name","Your age is");
-alert (x);
+//var x = prompt ("Enter your name","Your age is");
+//alert (x);
 
-var y = prompt ("Enter your name","Your age is");
-var z = y + 10;
-console.log(z);
+//var y = prompt ("Enter your name","Your age is");
+//var z = y + 10;
+//console.log(z);
 
-var c = +prompt ("Enter your name","Your age is");
-var d = c + 20; 
-console.log(d);
+//var c = +prompt ("Enter your name","Your age is");
+//var d = c + 20; 
+//console.log(d);
 
-var f = 10;
-document.write("The output is" + "" + f);
+//var f = 10;
+//document.write("The output is" + "" + f);
+
+var age = +prompt ("Enter your age");
+if(age == 14){
+    alert("Your age is equal")
+}
+else if("< 14") {
+    alert ("Your age is less")
+}
+else{
+    alert ("Your age is more")
+}
